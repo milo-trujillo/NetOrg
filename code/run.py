d947c2f397bfbe78b0508c55b68217bd7992dc70
Milo Trujillo <milo.trujillo@daylightingsociety.org>
2017-06-19 11:21:06 -0600

    Added additional trials

--- code/run.py
+++ code/run.py
@@ -37,13 +37,36 @@ parameters3 = {"innoise" : 10, # Stddev on incomming messages
               "envobsnoise" : 2, # Stddev on observing environment
               "batchsize" : 1000} # Training Batch Size
 
-parameters = [parameters1, parameters2, parameters3]
+# There are twice as many environment nodes to listen to
+parameters4 = {"innoise" : 2, # Stddev on incomming messages
+              "outnoise" : 2, # Stddev on outgoing messages
+              "num_environment" : 10, # Num univariate environment nodes
+              "num_agents" : 10, # Number of Agents
+              "fanout" : 1, # Distinct messages an agent can say
+              "statedim" : 1, # Dimension of Agent State
+              "envnoise": 25, # Stddev of environment state
+              "envobsnoise" : 2, # Stddev on observing environment
+              "batchsize" : 1000} # Training Batch Size
+
+# There are twice as many agent nodes as normal
+parameters5 = {"innoise" : 2, # Stddev on incomming messages
+              "outnoise" : 2, # Stddev on outgoing messages
+              "num_environment" : 5, # Num univariate environment nodes
+              "num_agents" : 20, # Number of Agents
+              "fanout" : 1, # Distinct messages an agent can say
+              "statedim" : 1, # Dimension of Agent State
+              "envnoise": 25, # Stddev of environment state
+              "envobsnoise" : 2, # Stddev on observing environment
+              "batchsize" : 1000} # Training Batch Size
+
+parameters = [parameters1, parameters2, parameters3, parameters4, parameters5]
 
 if __name__ == "__main__":
     plt.ion()
     orgs = []
     results = []
     fix,ax = plt.subplots()
+    #for i in ([0]):
     for i in range(len(parameters)):
         p = parameters[i]
         org = network.Organization(**p)
