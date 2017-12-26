3b1d1c3ded681da150191d7af815f417e7cd5b9f
Milo Trujillo <milo.trujillo@daylightingsociety.org>
2017-12-25 17:01:22 -0800

    Stripped deprecated code, improved documentation

--- code/agent.py
+++ code/agent.py
@@ -9,10 +9,14 @@ class Agent(object):
     An agent in an organization
 
     Note to self: tf.get_variable will *create* variables if they don't exist already!
+
+    In the nonlinear model agents are just a wrapper around one vector,
+    their "out weights". This vector is responsible for determining what they
+    listen to and how they translate those observations in to outgoing messages
     
     """
     _ids = count(0)
-    def __init__(self, noiseinstd, noiseoutstd, num, fanout, statedim, batchsize, numagents, numenv, prevState=None, **kwargs):
+    def __init__(self, noiseinstd, noiseoutstd, num, fanout, statedim, batchsize, numagents, numenv, **kwargs):
         self.id = next(self._ids)
         self.num = num
         self.statedim = statedim
@@ -22,36 +26,23 @@ class Agent(object):
         self.batchsize = batchsize
         self.numagents = numagents
         self.numenv = numenv
-        self.prevState = prevState
-        self.predecessor = None
         self.received_messages = None
 
-    def create_in_vec(self, indim):
-        print "Creating in vec for agent %d" % self.num
-        self.indim = indim
-        # If A0_1 is agent 15 and there's 10 agents, then A0_0 will be agent 5, but will be LISTEN_WEIGHT[prev_gen + num_env]
-        prev_gen = self.num - self.numagents
-        prev_listen = prev_gen + self.numenv
-        self.listen_weights = tf.get_variable(dtype=tf.float64, name=str(self.num) + "listen" +str(self.id) , shape=[1, indim])
-        #with tf.Session() as sess:
-            #init = tf.global_variables_initializer()
-            #sess.run(init)
-            #print "Agent %d Created with listen_weights: %s" % (self.num, str(sess.run(self.listen_weights)))
-
-    def listen_cost(self):
-        return tf.reduce_sum(tf.abs(self.listen_weights))
-
-    def set_predecessor(self, agent):
-        self.predecessor = agent
-
+    # This is for multi-layer systems.
+    # In a multi-layer system, we say an agent inherits the observations of its
+    # predecessor, so you can make decisions based on your observations at any
+    # previous timestep. It is currently unused in nonlinear systems.
     def set_received_messages(self, msgs):
         self.received_messages = msgs
 
+    # Run once at startup in network.py to initialize the out weights and 
+    # (optionally) print debugging data
+    # Note: Indim is how many Independent Messages each agent can send
+    # In other words, if indim=2 then each agent can send two distinct
+    # messages based on their observations. This is currently nonsensical
+    # in the nonlinear model.
     def create_out_matrix(self, indim):
         self.out_weights = tf.get_variable(dtype=tf.float64, name=str(self.num) + "out" +str(self.id), shape=[indim, self.fanout])
-        '''
-        Indim is the number of independent messages we can send. We can send one
-        '''
         #with tf.Session() as sess:
             #init = tf.global_variables_initializer()
             #sess.run(init)
